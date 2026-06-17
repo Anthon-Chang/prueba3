dcd459dad59e242ad5af4dc9551e7aab2417dfeb
Anthon-Chang <168015068+Anthon-Chang@users.noreply.github.com>
2026-06-17 16:21:16 -0500

    Add files via upload

---
+++ formulario/web1/app2.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+
+app = Flask(__name__)
+
+conexion = mysql.connector.connect(
+    host="mysql_secundario",
+    user="root",
+    password="root",
+    database="informacion"
+)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+
+@app.route("/agregar", methods=["POST"])
+def agregar():
+    nombre = request.form["nombre"]
+    correo = request.form["correo"]
+    formacion = request.form["formacion"]
+    experiencia = request.form["experiencia"]
+
+    cursor = conexion.cursor()
+    cursor.execute(
+        "INSERT INTO informacion (nombre, correo, formacion,experiencia) VALUES (%s, %s, %s,%s)",
+        (nombre, correo, formacion,experiencia)
+    )
+    conexion.commit() # Guarda permanentemente los cambios realizados en la base de datos.
+
+    return redirect("/") #envía al navegador nuevamente a la ruta principal
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
